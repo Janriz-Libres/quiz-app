6558874ef53afc11b4148894f78e75a09aa9ce7c
Janriz Libres <libres.janriz@gmail.com>
2024-11-05 10:30:39 +0800

    add questions and select

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,29 @@
+import QUESTIONS from "../questions";
+import { useState } from "react";
+
 export default function Quiz() {
-  const [question, setQuestion] = useState(0);
   const [answers, setAnswers] = useState([]);
 
-  return <p>Currently active question</p>;
+  const questionIdx = answers.length;
+
+  function handleSelectAnswer(answer) {
+    setAnswers((prev) => [...prev, answer]);
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <p>{QUESTIONS[questionIdx].text}</p>
+        <ul id="answers">
+          {QUESTIONS[questionIdx].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
